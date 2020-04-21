04e3d7bd2bdaa4d6cd01647a1538accd782b19b1
bmalkmus <bmalkmus23@gmail.com>
2020-04-21 14:28:03 -0700

    initial cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import employees from "./employees.json";
+import EmployeeCard from "./components/EmployeeCard";
+import Title from "./components/Title";
+import Wrapper from "./components/Wrapper";
 
-function App() {
+class App extends Component {
+state = {
+  employees
+}
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Title>Avengers Employee Directory</Title>
+      {this.state.employees.map(employee => (
+        <EmployeeCard 
+          id ={employee.id}
+          key = {employee.id}
+          hero = {employee.hero}
+          name = {employee.name}
+          image = {employee.image}
+          strength = {employee.strength}
+          />
+      ))}
+    </Wrapper>
+  )
+}
+
 }
 
 export default App;
---
+++ src/components/EmployeeCard/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeCard(props) {
+    return (
+        <div className = "card">
+            <div className = "img-container">
+                <img 
+                    alt = {props.hero} 
+                    src={props.image}
+                />
+            </div>
+            <div className ="card-body">
+                <h4 className = "card-title">
+                     {props.hero} 
+                </h4>
+                <p className = "card-text">
+                    Name: {props.name} 
+                </p>
+                <p className = "card-text">
+                    Strength: {props.strength} 
+                </p>
+            </div>
+
+        </div>
+    );
+}
+
+export default EmployeeCard
