82867a8a5ae307df0037132acb0089c8e8d95ebb
bmalkmus <bmalkmus23@gmail.com>
2020-04-26 00:41:24 -0700

    finished deployment

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import logo from "./Shield.png"
 
 function Title(props) {
     return (
         <div className = "title">
         <h1 className = "title-text">
-            <img src ="../Images/Shield.png" className ="logo lg1" alt = "logo"/>  {props.children}  <img src ="../Images/Shield.png" className ="logo lg2" alt = "logo"/>
+            <img src ={logo} className ="logo lg1" alt = "logo"/>  {props.children}  <img src ={logo} className ="logo lg2" alt = "logo"/>
         </h1>
         </div>
 
